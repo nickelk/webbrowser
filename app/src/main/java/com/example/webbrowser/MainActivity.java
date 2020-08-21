7e6c7fb64e9811cb86d40a2e61cd056a4678b2bc
NSmith <nick_smith@ukr.net>
2020-08-21 21:23:13 +0300

    it works

--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         WebSettings ws = web.getSettings();
         ws.setJavaScriptEnabled(true);
         web.loadUrl("https://itproger.com");
+        web.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(web.canGoBack())
+            web.goBack();
+        else
+        super.onBackPressed();
     }
 }
