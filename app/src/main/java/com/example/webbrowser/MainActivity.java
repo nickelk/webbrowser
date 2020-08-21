91b155e526c99c1d30a9c16b0012466c9c050658
NSmith <nick_smith@ukr.net>
2020-08-21 11:08:14 +0300

    webview code added

--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.webbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        web = (WebView)findViewById(R.id.webView);
+        WebSettings ws = web.getSettings();
+        ws.setJavaScriptEnabled(true);
+        web.loadUrl("https://itproger.com");
     }
 }
